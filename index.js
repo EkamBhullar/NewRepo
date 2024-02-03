405f5f4776a4b7874f8d96662dd61f2cfc6bc990
Developer-vansh <117535964+Developer-vansh@users.noreply.github.com>
2024-02-03 21:29:14 +0530

    New

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const app=express();
 const port=process.env.PORT;
 app.get('/',(req,res)=>{
-    res.send("HII VAnsh GAba")
+    res.send("HII VAnsh GAba Deploy")
 })
 app.listen(3000,()=>{
     console.log("App listening", 3000)
