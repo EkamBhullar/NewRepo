d59cf49171cc7990c5855e65471fa14c972ae030
Developer-vansh <117535964+Developer-vansh@users.noreply.github.com>
2024-02-03 20:31:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express=require('express');
+require('dotenv').config()
+const app=express();
+const port=process.env.PORT;
+app.get('/',(req,res)=>{
+    res.send("HII VAnsh GAba")
+})
+app.listen(3000,()=>{
+    console.log("App listening", 3000)
+})
+
