f3b6022b365238c1c55b322c8a0cf7ce21438984
Developer-vansh <117535964+Developer-vansh@users.noreply.github.com>
2024-02-04 12:27:03 +0530

    new

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express=require('express');
 require('dotenv').config()
+const path = require('path');
 const app=express();
 const port=process.env.PORT;
+app.use(express.static("public"));
 app.get('/',(req,res)=>{
-    res.send("HII VAnsh GAba Deploy Updated ")
+    res.sendFile(__dirname + '/public/index.html');
 })
+app.get('/style.css', function(req, res) {
+    res.sendFile(path.join(__dirname, '/public/style.css'));
+});
 app.listen(8082,()=>{
     console.log("App listening", 8082)
 })
