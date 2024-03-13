f72d6ae523db4ca155171a0726c02240d97cc37a
Ekamjot Singh Bhullar <bhullarekam04@gmail.com>
2024-03-13 22:12:16 +0530

    updated

--- test.js
+++ test.js
@@ -3,13 +3,13 @@ const assert = require('assert');
 const app = require('./testserver');
 
 describe('GET /', () => {
-  test('It should respond with status code 200',  () => {
-    const response =  request(app).get('/');
+  test('It should respond with status code 200', async () => {
+    const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
   });
 
-  test('It should respond with HTML content', () => {
-    const response =  request(app).get('/');
+  test('It should respond with HTML content',async () => {
+    const response =  await request(app).get('/');
     expect(response.headers['content-type']).toContain('text/html');
   });
 });
