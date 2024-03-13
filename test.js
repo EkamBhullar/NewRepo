e67194733d341b8e260eade27469a500dd7ca0f9
Ekamjot Singh Bhullar <123894878+EkamBhullar@users.noreply.github.com>
2024-03-13 20:25:59 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,39 +1,26 @@
 const request = require('supertest');
-const assert = require('assert');
-const app = require('./testserver');
+const app = require('./testserver'); // Replace 'your-express-app-file' with the name of your Express app file
 
-describe('Unit Testing ', function() {
-    it('should return index.html on GET /', function(done) {
-                request(app)
-                    .get('/')
-                    .expect(200)
-                    .end(function(err, res) {
-                        if (err) return done(err);
-                        assert.strictEqual(res.type, 'text/html');
-                        done();
-                    });
-            });
+describe('GET /', () => {
+  test('It should respond with status code 200', async () => {
+    const response = await request(app).get('/');
+    expect(response.statusCode).toBe(200);
+  });
 
-    it('should return style.css on GET /style.css', function(done) {
-        request(app)
-            .get('/style.css')
-            .expect(200)
-            .end(function(err, res) {
-                if (err) return done(err);
-                assert.strictEqual(res.type, 'text/css');
-                done();
-            });
-    });
-    it('should have a valid HTML structure', function(done) {
-        request(app)
-            .get('/')
-            .expect(200)
-            .end(function(err, res) {
-                if (err) return done(err);
-                // Check if the response body contains the head and body tags
-                assert(res.text.includes('<head>') && res.text.includes('</head>'));
-                assert(res.text.includes('<body>') && res.text.includes('</body>'));
-                done();
-            });
-    });
+  test('It should respond with HTML content', async () => {
+    const response = await request(app).get('/');
+    expect(response.headers['content-type']).toContain('text/html');
+  });
+});
+
+describe('GET /style.css', () => {
+  test('It should respond with status code 200', async () => {
+    const response = await request(app).get('/style.css');
+    expect(response.statusCode).toBe(200);
+  });
+
+  test('It should respond with CSS content', async () => {
+    const response = await request(app).get('/style.css');
+    expect(response.headers['content-type']).toContain('text/css');
+  });
 });
